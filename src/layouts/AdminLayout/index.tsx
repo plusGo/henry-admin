f48e6cb3b5c674089017ae5123c08e9b90add5d8
menghonglin <mhlplus@outlook.com>
2021-08-30 23:04:49 +0800

    feat:admin layout

--- src/layouts/AdminLayout/index.tsx
+++ src/layouts/AdminLayout/index.tsx
@@ -1,7 +1,55 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
 
 const AdminLayout: FC = (props) => {
-  return <div>{props.children}</div>;
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout className="w-screen h-screen">
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        className="relative"
+      >
+        <div className="text-white text-3xl flex items-center justify-center py-8 font-bold">
+          {collapsed ? 'H' : 'Henry Admin'}
+        </div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            欢迎
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            功能管理
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
+        </Menu>
+        {React.createElement(
+          collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+          {
+            className: ' absolute bottom-10 left-10 cursor-pointer text-white',
+            style: { color: '#fff' },
+            onClick: () => setCollapsed(!collapsed),
+          },
+        )}
+      </Sider>
+      <Layout>
+        <Header className="bg-white shadow h-20"></Header>
+        <Content className="overflow-auto">{props.children}</Content>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default AdminLayout;
